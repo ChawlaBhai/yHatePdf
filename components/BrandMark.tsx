c256dec93a8bc8bf995d09c3017cee5789cc9445
ChawlaBhai <sahajsadhu@gmail.com>
2026-09-22 13:46:20 +0530

    Fix logo rendering overflow

--- components/BrandMark.tsx
+++ components/BrandMark.tsx
@@ -1,13 +1,12 @@
 export default function BrandMark({ className = "" }: { className?: string }) {
   return <span className={`brand-mark ${className}`} aria-label="Why hate PDF?">
     <span className="brand-word">WHY</span>
-    <svg className="brand-hate" viewBox="0 0 134 44" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-      <g transform="skewX(-5)">
-        <text x="4" y="35" className="brand-hate-fill">HATE</text>
-        <text x="4" y="35" className="brand-hate-stroke">HATE</text>
-        <path d="M28 6l-5 12 8-3-6 20M58 4l-7 13 9-3-5 23M88 5l-5 11 8-2-7 22M116 8l-7 10 8-2-5 18" className="brand-crack"/>
-      </g>
-    </svg>
+    <span className="brand-hate" aria-hidden="true">
+      <span className="brand-hate-word">HATE</span>
+      <svg viewBox="0 0 100 38" xmlns="http://www.w3.org/2000/svg" focusable="false">
+        <path d="M18 3l-4 12 7-3-5 22M42 2l-5 13 8-3-6 23M67 3l-5 12 8-3-6 22M90 5l-5 10 7-2-5 19"/>
+      </svg>
+    </span>
     <span className="brand-letter">PDF?</span>
   </span>;
 }
